851927c28cc6b3ec5ae98945d086d7a4cf29f62b
NadinDesyatova <nadindinina@yandex.ru>
2024-11-11 20:28:35 +0300

    unnecessary files has deleted

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,5 @@
-import Team from "./team";
-import Character from "./character";
 import canIterate from "./canIterate";
 
-
-const firstCharacter = new Character('Neo', 'Undead');
-const secondCharacter = new Character('John', 'Swordsman');
-
-const team = new Team();
-
-team.addAll(firstCharacter, secondCharacter);
-const teamIterator = team[Symbol.iterator]();
-console.log(teamIterator.next());
-console.log(teamIterator.next());
-console.log(teamIterator.next());
 
 console.log(canIterate(new Map())); 
 console.log(canIterate(new Set())); 
